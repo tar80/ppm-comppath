1cb967d3c48b0183e7b33a4ed0e3cdb1e51d47cc
tar80 <tar80master@gmail.com>
2024-09-22 19:38:19 +0900

    feat(staymode): 再度常駐できるように変更した

--- src/trimEnd.stay.ts
+++ src/trimEnd.stay.ts
@@ -1,13 +1,22 @@
 /* @file Complete the path being edited to the upper level path
+ * @arg 0 {number} - If non-zero, enable StayMode
  */
 
 import {hasArg} from '@ppmdev/modules/argument.ts';
 import debug from '@ppmdev/modules/debug.ts';
 import {isBottom} from '@ppmdev/modules/guard.ts';
-import {atActiveEvent} from '@ppmdev/modules/staymode.ts';
 
 const DELIM = '@##@';
-const EVENT_LABEL = 'ppm_comppath';
+
+const main = (): void => {
+  const isStaymode = hasArg('1');
+
+  if (isStaymode) {
+    PPx.StayMode = 2;
+  }
+
+  ppx_resume();
+};
 
 const ppx_resume = (): void => {
   const text = PPx.Extract('%*edittext');
@@ -23,8 +32,6 @@ const ppx_resume = (): void => {
   PPx.Execute(`*replace "%(${replacedText}%)"%:%k"@END"`);
 };
 
-// const ppx_finally = (): void => PPx.Echo('[ERROR] instance remain trimEnd.stay.js');
-
 const splitString = (v: string): string[] => {
   const rgx = /^([^/\\]+\s)?(.+(?:[,/\\ ]))(?!$).*/;
   const v_ = v.replace(rgx, (_, cmd, pwd) => {
@@ -36,5 +43,5 @@ const splitString = (v: string): string[] => {
   return v_.split(DELIM);
 };
 
-if (!debug.jestRun()) ppx_resume();
+if (!debug.jestRun()) main();
 // export {splitString}
